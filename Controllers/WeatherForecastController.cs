60a31b60b46df79064dad0ce628543044267d29a
Marcrows <marcoslanuza.t@gmail.com>
2022-08-01 21:18:38 -0600

    Configuring routes

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace APIProj.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+[Route("api/[controller]")]
 public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
@@ -32,7 +32,10 @@ public class WeatherForecastController : ControllerBase
     }
 
     [HttpGet(Name = "GetWeatherForecast")]
-    public IEnumerable<WeatherForecast> Get()
+    [Route("get/weatherforecast")]
+    [Route("get/weatherforecast2")]
+    [Route("[action]")]
+    public IEnumerable<WeatherForecast> Obtener()
     {
         return ListWeatherForecast;
     }

3de3e67de61cf16fd7516b8c8d6a2833c62a9f59
Marcrows <marcoslanuza.t@gmail.com>
2022-08-29 17:30:29 -0600

    Service for Categories

---
+++ Services/CategoriaService.cs
@@ -0,0 +1,56 @@
+using webapi.Models;
+
+namespace webapi.Services; 
+public class CategoriaService : ICategoriaService
+{
+    TareasContext context;
+
+    public CategoriaService(TareasContext dbcontext)
+    {
+        context = dbcontext;
+    }
+
+    public IEnumerable<Categoria> Get()
+    {
+        return context.Categorias;
+    }
+
+    public async Task Save(Categoria CategoriaObject)
+    {
+        context.Add(CategoriaObject);
+        await context.SaveChangesAsync();
+    }
+
+    public async Task Update(Guid Id, Categoria CategoriaObject)
+    {
+        var CategoriaActual = context.Categorias.Find(Id);
+
+        if (CategoriaActual != null)
+        {
+            CategoriaActual.Nombre = CategoriaObject.Nombre;
+            CategoriaActual.Descripcion = CategoriaObject.Descripcion;
+            CategoriaActual.Peso = CategoriaObject.Peso;
+
+            await context.SaveChangesAsync();
+        }
+    }
+
+    public async Task Delete(Guid Id)
+    {
+        var CategoriaActual = context.Categorias.Find(Id);
+
+        if (CategoriaActual != null)
+        {
+            context.Remove(CategoriaActual);
+
+            await context.SaveChangesAsync();
+        }
+    }
+}
+
+public interface ICategoriaService
+{
+    IEnumerable<Categoria> Get();
+    Task Update(Guid Id, Categoria CategoriaObject);
+    Task Delete(Guid Id);
+}
